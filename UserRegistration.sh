cc34137f4c9f362fd24884ad69cde0e4b1b27ec2
Shraddha2225 <shraddhat22m@gmail.com>
2020-02-17 13:09:56 +0530

    ADDED: Check For Valid Mobile Number Starts With Country Code

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -7,6 +7,7 @@ echo "Welcome To User Registration validation"
 firstName="^[A-Z]{1}[a-z]{3,}$"
 firstNameAndlastName="^[A-Z]{1}[a-z]{3,}[[:space:]][A-Z]{1}[a-z]{3,}$"
 validEmail="^[a-zA-Z]{3,}[+-._]*{1}[a-zA-Z]*{3,}[@]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,3}[.]*{1}[a-zA-Z]*{2,3}$"
+validMobileNumber="^[0-9]{2}[[:space:]][0-9]{10}$"
 
 function firstName()
 {
@@ -41,7 +42,19 @@ function validEmail()
 	fi
 }
 
+function mobileNumber()
+{
+	read -p "Enter Valid Mobile Number"  input
+	if [[ $input =~ $validMobileNumber ]]
+	then
+		echo "Valid Mobile Number"
+	else
+		echo "Please Enter A Valid Mobile Number"
+	fi
+}
+
 #calling function
 firstName
 FirstAndlastName
 validEmail
+mobileNumber
