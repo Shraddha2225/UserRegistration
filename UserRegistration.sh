3fb6ad94318de03d2fc307a753c67103352a3684
Shraddha2225 <shraddhat22m@gmail.com>
2020-02-17 12:54:08 +0530

    ADDED: Check Proper First Name With Minimum 8 Character

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,4 +3,18 @@
 shopt -s extglob
 #welcome message
 echo "Welcome To User Registration validation"
+#patterns
+firstName="^[A-Z]{1}[a-z]{3,}$"
+function firstName()
+{
+	read  -p  "Enter First Name" input
+	if [[ $input =~ $firstName ]]
+	then
+		echo "Valid First Name"
+	else
+		echo "Please Enter A Valid First Name"
+	fi
+}
 
+#calling function
+firstName
